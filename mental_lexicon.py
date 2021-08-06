5e5d5d4334972da9564d7ebf7b96721af01f120c
trenusch <trenusch@techfak.uni-bielefeld.de>
2021-08-06 07:52:48 +0200

    items can be changed to unknown @mental_lexicon.py

--- mental_lexicon.py
+++ mental_lexicon.py
@@ -8,13 +8,15 @@ class MentalLexicon:
 
     def add_item(self, item):
         item = eval(item)
-        if item not in self.items:
+        if item.value not in [concept.value for concept in self.items]:
             self.items.append(item)
             if item.known:
                 self.words.append(item.value)
         else:
-            self.items.remove(item)
+            self.items.remove(self.from_word(item.value))
             self.items.append(item)
+            if item.value in self.words:
+                self.words.remove(item.value)
             if item.known:
                 self.words.append(item.value)
         if type(item) == Attribute:
